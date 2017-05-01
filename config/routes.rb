6787e40c76ae4996cc9a3f76efac4c416603b8a9
arnavl <aluthra@andrew.cmu.edu>
2017-04-30 23:53:22 -0400

    authentication finished

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
   get 'privacy' => 'home#privacy', as: :privacy
   
   # Set the root url
-  root :to => 'home#home'  
+  root :to => 'home#home'    
+
+  resources :users
+  resources :sessions
+  get 'user/edit' => 'users#edit', :as => :edit_current_user
+  get 'signup' => 'users#new', :as => :signup
+  get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
+
 
 end
